b78393eb52b802e21a7ae2d4a85b4cc623003c64
Micah Njeru <micahnjeru1@gmail.com>
2024-09-22 18:18:20 +0300

    Instaled a virtual environment, PIL, numpy, pytesseract. Deleted jupyter folder and files. Moved the test image.

--- OCR-Scripts/ocr.py
+++ OCR-Scripts/ocr.py
@@ -1,5 +1,21 @@
-# Python Tesseract Script
+""" 
+Python Tesseract Script
+"""
 
+# Import necessary modules
 from PIL import Image
 import pytesseract
 import numpy as np
+
+"""
+Start of the script
+"""
+
+# Assign variable to name of test image
+filename = 'ImageTest.png'
+# Convert image into an numpy array
+img1 = np.array(Image.open(filename))
+# Use Tesseract to convert the image of numpy array to text
+text = pytesseract.image_to_string(img1)
+# Print the result
+print(text)
