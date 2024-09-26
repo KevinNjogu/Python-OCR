1c24c6930a687decb97cc300b20c08ba2b92f85b
Micah Njeru <micahnjeru1@gmail.com>
2024-09-26 10:54:12 +0300

    Cropped images in DOW-30 & S&P 500. Also added path to actual images to test.

--- OCR-Scripts/ocr.py
+++ OCR-Scripts/ocr.py
@@ -12,7 +12,7 @@ Start of the script
 """
 
 # Assign variable to name of test image
-filename = 'ImageTest.png'
+filename = 'images_source/DOW-30/DOW 30_page-0001.jpg'
 # Convert image into an numpy array
 img1 = np.array(Image.open(filename))
 # Use Tesseract to convert the image of numpy array to text
